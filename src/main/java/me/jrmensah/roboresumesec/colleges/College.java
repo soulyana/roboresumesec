2354c6fa3dfe0e666f666d4e9d32bb2ca5d9d40c
jrmensah <jerilynmensah@gmail.com>
2017-11-22 14:40:02 -0500

    Added files from 'RoboResume' project

---
+++ src/main/java/me/jrmensah/roboresumesec/colleges/College.java
@@ -0,0 +1,41 @@
+package me.jrmensah.roboresumesec.colleges;
+
+public class College {
+    public String degree;
+    public String major;
+    public String name;
+    public int year;
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
+
